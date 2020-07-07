c000875a9d738237c7a7f9e6f581bb2257ba67f1
Th3.C0d3r-418 = Rafael Luis <rafaellfra@hotmail.com>
2020-07-07 13:06:32 -0300

    Alteração das cores

--- src/Components/Installment.js
+++ src/Components/Installment.js
@@ -4,16 +4,18 @@ import './Installments.css';
 
 export default function Installment({ objInstallment, mes }) {
     const { montante, diffMontante, juros } = objInstallment;
+    const textDinamicColorJuros = 'gridValue text-lighten-1 ' + (juros > 0 ? 'blue-text' : juros < 0 ? 'orange-text' : 'grey-text');
+    const textDinamicColorValues = 'gridValue text-lighten-1 ' + (juros > 0 ? 'green-text' : juros < 0 ? 'red-text' : 'grey-text');
     return (
         <div className="boxCard">
             <div className="gridCard">
                 <div className="gridMonth teal darken-3 grey-text text-lighten-3">{mes}</div>
-                <div className="gridInstallments">
-                    <div className="gridValue">R$ {formatNumber(montante)}</div>
-                    <div className="gridValue ">
+                <div className="gridInfo">
+                    <div className={textDinamicColorValues}>R$ {formatNumber(montante)}</div>
+                    <div className={textDinamicColorValues}>
                         {diffMontante > 0 ? '+' : diffMontante < 0 ? '-' : ''} R$ {formatNumber(Math.abs(diffMontante))}
                     </div>
-                    <div className="gridValue">{formatNumber(juros)}%</div>
+                    <div className={textDinamicColorJuros}>{formatNumber(juros)}%</div>
                 </div>
             </div>
         </div>
